8213471a2ba19a8daa1ee7671b4c9b79a2031879
Antoine Eiche <lewo@abesis.fr>
2020-06-16 11:26:13 +0200

    Update flake-compat library
    flake-compat now tries to fetchGit to clean up the source tree, if the
    source tree is a valid Git repository.
    
    The CI no longer has to checkout a full Git repository: the default
    shallow clone is now sufficient.
    
    Fixes #1370

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 (import (
   fetchTarball {
-    url = https://github.com/edolstra/flake-compat/archive/f012cc5092f01fc67d5cd6f04999f964c3e05cbf.tar.gz;
-    sha256 = "1n8q7v7alq802kl3b6zan6v27whi2ppbnlv8df6cz64vqf658ija"; }) {
-      src = builtins.fetchGit ./.;
+    url = https://github.com/nlewo/flake-compat/archive/968fc5cee2baf5426ff895d6d69066723b54174d.tar.gz;
+    sha256 = "0aalg5pdl1j3g73jlglcnj1jxi37iyzbnsjcwx3m0iphl95q28qg"; }) {
+      src = ./.;
 }).shellNix
